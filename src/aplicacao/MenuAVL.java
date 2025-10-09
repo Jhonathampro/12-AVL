a4206661a42373fb6d32d7bd3b2a239d672723f6
Laboratório FIAP <labsfiap@fiap.com.br>
2025-10-09 09:30:33 -0300

    feat: adicionar o metodo de remover

--- src/aplicacao/MenuAVL.java
+++ src/aplicacao/MenuAVL.java
@@ -14,6 +14,8 @@ public class MenuAVL {
 			System.out.println("0 - Sair");
 			System.out.println("1 - Inserir");
 			System.out.println("2 - Mostra Fator de Balanceamento");
+			System.out.println("3 - Remover");
+			System.out.println("4 - conta nos");
 			opcao = le.nextInt();
 			switch (opcao) {
 			case 0:
@@ -29,6 +31,15 @@ public class MenuAVL {
 				System.out.println("Apresentacao da AVL");
 				avl.mostrarFP(avl.root);
 				break;
+				case 3:
+					System.out.println("Informe o valor a ser removido");
+					valor = le.nextInt();
+					avl.root = avl.removeValor(avl.root, valor);
+					break;
+				case 4:
+					System.out.println(avl.contaNos(avl.root, 0));
+					break;
+
 			default:
 				System.out.println("Opcao invalida");
 			}
